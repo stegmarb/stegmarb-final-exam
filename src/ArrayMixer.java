2b96f62be01ae0f74adf790b11d3c610fd2f789b
Balazs Stegmar <stegmarb@gmail.com>
2017-07-10 10:03:57 +0200

    feat(ArrayMixer): add parts which can handle non equal length sised arrays

--- src/ArrayMixer.java
+++ src/ArrayMixer.java
@@ -2,9 +2,29 @@ public class ArrayMixer {
 
   public int[] mixTwoArrays(int[] firstArray, int[] secondArray) {
     int[] resultArray = new int[firstArray.length + secondArray.length];
-    for (int i = 0; i < firstArray.length; i++) {
-      resultArray[i * 2] = firstArray[i];
-      resultArray[(i *2) + 1] = secondArray[i];
+    if (firstArray.length == secondArray.length) {
+      for (int i = 0; i < firstArray.length; i++) {
+        resultArray[i * 2] = firstArray[i];
+        resultArray[(i * 2) + 1] = secondArray[i];
+      }
+    } else if (firstArray.length > secondArray.length) {
+      for (int i = 0; i < firstArray.length; i++) {
+        if (i < secondArray.length) {
+          resultArray[i * 2] = firstArray[i];
+          resultArray[(i * 2) + 1] = secondArray[i];
+        } else {
+          resultArray[i + secondArray.length] = firstArray[i];
+        }
+      }
+    } else {
+      for (int i = 0; i < secondArray.length; i++) {
+        if (i < firstArray.length) {
+          resultArray[i * 2] = firstArray[i];
+          resultArray[(i * 2) + 1] = secondArray[i];
+        } else {
+          resultArray[i + firstArray.length] = secondArray[i];
+        }
+      }
     }
     return resultArray;
   }
